de0c23144634af8b681f025f116d445c4b0ec555
adrianohermida <adrianohermida@gmail.com>
2026-01-22 15:26:10 -0400

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { cloudflare } from '@cloudflare/vite-plugin';
 import { reactComponentTagger } from 'react-component-tagger';
 
 export default defineConfig({
-  base: '/heyboss_fix/',
+  base: '/',
   plugins: [react(), reactComponentTagger() as PluginOption, cloudflare()],
   server: {
     proxy: {
